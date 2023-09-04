f36f3768b836fefe7e72ae2fc36681cc0227ca73
Ali Al Sules <3lims96@gmail.com>
2023-09-04 09:39:33 +0300

    api key secured

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -4,41 +4,40 @@ import { createContext } from "react";
 export const MoviesContext = createContext();
 
 export const MoviesProvider = ({ children }) => {
-    const key = import.meta.env.VITE_SOME_KEY;
 
     const getTrendingMovies = async (pageNumber) => {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&page=${pageNumber}`);
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_SOME_KEY}&page=${pageNumber}`);
         const data = await response.json();
         return data.results;
     }
 
     const getTopMovies = async (pageNumber) => {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&page=${pageNumber}`);
+        const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${import.meta.env.VITE_SOME_KEY}&page=${pageNumber}`);
         const data = await response.json();
         return data.results;
     }
 
 
     const getTrendingAnime = async (pageNumber) => {
-        const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${key}&with_original_language=ja&with_genres=16&page=${pageNumber}`);
+        const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_SOME_KEY}&with_original_language=ja&with_genres=16&page=${pageNumber}`);
         const data = await response.json();
         return data.results;
     }
 
     const getTopAnime = async (pageNumber) => {
-        const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&with_original_language=ja&with_genres=16&page=${pageNumber}`);
+        const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${import.meta.env.VITE_SOME_KEY}&with_original_language=ja&with_genres=16&page=${pageNumber}`);
         const data = await response.json();
         return data.results;
     }
 
     const getTrendingShows = async (pageNumber) => {
-        const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${key}&without_keywords=210024&page=${pageNumber}`);
+        const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_SOME_KEY}&without_keywords=210024&page=${pageNumber}`);
         const data = await response.json();
         return data.results;
     }
 
     const getTopShows = async (pageNumber) => {
-        const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&without_keywords=210024&page=${pageNumber}`);
+        const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${import.meta.env.VITE_SOME_KEY}&without_keywords=210024&page=${pageNumber}`);
         const data = await response.json();
         return data.results;
     }
@@ -46,7 +45,7 @@ export const MoviesProvider = ({ children }) => {
 
     const getMovieDetails = async (id) => {
         let response;
-        response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}&append_to_response=videos`);
+        response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_SOME_KEY}&append_to_response=videos`);
 
         const data = await response.json();
         return data;
@@ -54,7 +53,7 @@ export const MoviesProvider = ({ children }) => {
 
     const getTvDetails = async (id) => {
         let response;
-        response = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${key}&append_to_response=videos`);
+        response = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${import.meta.env.VITE_SOME_KEY}&append_to_response=videos`);
 
         const data = await response.json();
         return data;
@@ -62,19 +61,19 @@ export const MoviesProvider = ({ children }) => {
 
 
     const getMovieSearchResults = async (text) => {
-        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${text}`);
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_SOME_KEY}&query=${text}`);
         const data = await response.json();
         return data.results;
     }
 
     const getShowSearchResults = async (text) => {
-        const response = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${key}&without_keywords=210024&query=${text}`);
+        const response = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${import.meta.env.VITE_SOME_KEY}&without_keywords=210024&query=${text}`);
         const data = await response.json();
         return data.results;
     }
 
     const getAnimeSearchResults = async (text) => {
-        const response = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${key}&with_original_language=ja&with_genres=16&query=${text}`);
+        const response = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${import.meta.env.VITE_SOME_KEY}&with_original_language=ja&with_genres=16&query=${text}`);
         const data = await response.json();
         return data.results;
     }
